f17a5c982a35eff8668a5b4341944bd49da18342
Klas Segeljakt <klasseg@kth.se>
2023-03-18 15:54:34 +0100

    Replace walkdir with ignore to ignore target dir

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::Error;
 use std::io::Write;
 use std::path::Path;
 
-use walkdir::WalkDir;
+use ignore::WalkBuilder;
 
 fn main() -> Result<(), Error> {
     let root = std::env::current_dir()?;
@@ -14,33 +14,38 @@ fn main() -> Result<(), Error> {
 }
 
 fn read_dir(path: &Path, root: &Path, writer: &mut impl Write) -> Result<(), std::io::Error> {
-    for entry in WalkDir::new(path).into_iter().filter_map(Result::ok) {
-        if entry.file_type().is_file() {
+    let walk = WalkBuilder::new(path)
+        .filter_entry(|e| {
+            e.file_name()
+                .to_str()
+                .map(|s| !s.starts_with('.') && s != "target")
+                .unwrap_or(false)
+        })
+        .build();
+
+    for entry in walk.filter_map(Result::ok) {
+        if entry
+            .file_type()
+            .expect("Failed to get file type")
+            .is_file()
+        {
             let Some(ext) = entry.path().extension() else { continue };
             let Some(ext) = ext.to_str() else { continue };
             if ext == "rs" || ext == "md" || entry.file_name() == "Cargo.toml" {
-                read_file(entry.path(), root, ext, writer)?;
+                read_file(entry.path(), root, writer)?;
             }
         }
     }
     Ok(())
 }
 
-fn read_file(
-    path: &Path,
-    root: &Path,
-    ext: &str,
-    writer: &mut impl Write,
-) -> Result<(), std::io::Error> {
+fn read_file(path: &Path, root: &Path, writer: &mut impl Write) -> Result<(), std::io::Error> {
     let mut file = File::open(path)?;
-    writeln!(writer, "```{ext}")?;
     let path = path
         .strip_prefix(root)
         .expect("Failed to strip prefix")
         .display();
     writeln!(writer, "// {path}")?;
     std::io::copy(&mut file, writer)?;
-
-    writeln!(writer, "```")?;
     Ok(())
 }
